5a3b06fe40c2c3518509fc8ee2e01561f1248c09
Esquire5100 <KLEAH.24@ichat.sp.edu.sg>
2025-05-14 13:53:21 +0800

    updated script

--- Assets/Scripts/Scottie_Controller.cs
+++ Assets/Scripts/Scottie_Controller.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Charactermovementscript : MonoBehaviour
 {
+    public float speed = 5; //public -> can see in unity
+    private float movement; //private -> can only be seen/changed/referenced to in the script
+
+    new public Rigidbody2D rigidbody; //make reference to the Rigidbody2D component
+
+    //Ground detection variables
+    public Transform groundCheck; //A pt in space to check where the ground is
+    public float groundCheckRadius;
+    public LayerMask realGround;
+    public bool isGrounded;
+    private float horizontalInput;
+
+    private Animator myAnim; //Store a ref to animtions to access later
+
+    public Vector3 respawnPosition; //Store a respawn pos the player will go to whenever she dies
+
     // Start is called before the first frame update
     void Start()
     {
