a4a08cbb5e551be6edf38b200d0e408e879eb40b
M-okyt <Kaiyangt2003@gmail.com>
2025-05-28 14:48:04 +0800

    Security Camera

--- Assets/Scripts/SecurityCamera.cs
+++ Assets/Scripts/SecurityCamera.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class SecurityCamera : MonoBehaviour
 {
-    public SpriteRenderer sr;
+    public SpriteRenderer sr;                           //Sprite render reference
+
+    //Public Reference to Camera sprites
+    public Sprite leftCamera;                           //left
+    public Sprite centreCamera;                         //centre
+    public Sprite rightCamera;                          //right
 
 
     // Start is called before the first frame update
-    void Start()
+    void Start()        
     {
-        sr = GetComponent<SpriteRenderer>();
+        sr = GetComponent<SpriteRenderer>();            //Get and storing reference to component
+        StartCoroutine(ChangeSpriteAfterDelay());       //State it exist i think
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    IEnumerator ChangeSpriteAfterDelay()                //Make sprite change after specified times        
+    {
+        for(; ; )
+        {
+            yield return new WaitForSeconds(3f);
+            sr.sprite = leftCamera;
+            yield return new WaitForSeconds(3f);
+            sr.sprite = centreCamera;
+            yield return new WaitForSeconds(3f);
+            sr.sprite = rightCamera;
+        }
         
     }
 }
