2f8d17e2fa9a237da0d9e49d74e8545b5cac9794
Moky-t <Kaiyangt.24@ichat.sp.edu.sg>
2025-05-29 18:07:42 +0800

    Wokring on mobile controls, saving to let nina upload spritesheet

--- Assets/Scripts/Scottie_Controller.cs
+++ Assets/Scripts/Scottie_Controller.cs
@@ -5,28 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Scottie_Controller : MonoBehaviour
 {
-    public float MoveSpeed = 5f; //how fast the character moves
+    public float MoveSpeed = 5f;                                                 //how fast the character moves
 
-    private Rigidbody2D rb; //make a ref to the rigidbody2D component
+    private Rigidbody2D rb;                                                      //make a ref to the rigidbody2D component
     private SpriteRenderer sr;
     private float movement;
 
-    private bool isHidden = false; //is 'false' by default because we want the default tag to be "Player"
+    private bool isHidden = false;                                               //is 'false' by default because we want the default tag to be "Player"
 
-    private Animator myAnim; //Store a ref to animtions to access later
+    private Animator myAnim;                                                     //Store a ref to animtions to access later
 
-    AudioManager audioManager; //reference the Audio Manager Script
+    AudioManager audioManager;                                                   //reference the Audio Manager Script
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); //Get easy access to the Rigidbody2D component
-        sr = GetComponent<SpriteRenderer>(); //Get easy access to the SpriteRenderer component
-        myAnim = GetComponent<Animator>(); //Get easy access to the Animator component
+        rb = GetComponent<Rigidbody2D>();                                        //Get easy access to the Rigidbody2D component
+        sr = GetComponent<SpriteRenderer>();                                     //Get easy access to the SpriteRenderer component
+        myAnim = GetComponent<Animator>();                                       //Get easy access to the Animator component
     }
 
     void Update()
     {
-        //Get horizontal input (-1 for left, 1 for right, 0 for none)
+        /*Get horizontal input (-1 for left, 1 for right, 0 for none)
         movement = Input.GetAxisRaw("Horizontal");
 
         //For making sure the character faces the direction it's moving in
@@ -46,8 +46,8 @@ public class Scottie_Controller : MonoBehaviour
         if (Input.GetKeyDown("e"))
         {
             isHidden = !isHidden; //Changes 'isHidden' to true
-            gameObject.tag = isHidden ? "Hidden" : "Player"; //The "?" means "value if true" : "value if false". It's cleaner than a lot of if-else statements
-        }
+            gameObject.tag = isHidden ? "Hidden" : "Player";                    //The "?" means "value if true" : "value if false". It's cleaner than a lot of if-else statements
+        }*/
 
         //Setting up Parameters in the Animator
         myAnim.SetFloat("MoveSpeed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
@@ -56,7 +56,7 @@ public class Scottie_Controller : MonoBehaviour
     void FixedUpdate()
     {
         // Move the character left or right
-        rb.velocity = new Vector2(movement * MoveSpeed, rb.velocity.y);
+        //rb.velocity = new Vector2(movement * MoveSpeed, rb.velocity.y);
     }
 
     //Audio Scripts
@@ -65,4 +65,35 @@ public class Scottie_Controller : MonoBehaviour
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
         //Add "audioManager.PlaySFX(audioManager.XX);" to movements/hacking etc
     }
+
+    public void Move(float dir)
+    {
+        //Get horizontal input (-1 for left, 1 for right, 0 for none)
+        //movement = Input.GetAxisRaw("Horizontal");
+
+        //For making sure the character faces the direction it's moving in
+        if (dir > 0)
+        {
+            //moving left -> sprite flips to the left
+            //sr.flipX = true;
+            rb.velocity = new Vector2(MoveSpeed, rb.velocity.y);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (dir < 0)
+        {
+            rb.velocity = new Vector2(-MoveSpeed, rb.velocity.y);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        /*if (dir > 0)
+        {
+            //moving right -> sprite doesnt flip
+            sr.flipX = false;
+        }*/
+    }
+    
 }
