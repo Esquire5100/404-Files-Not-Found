59932ad685a58a63ed69b2e8444b9d5b0e47e918
Moky-t <Kaiyangt.24@ichat.sp.edu.sg>
2025-05-29 22:30:34 +0800

    FINISHED MOBILE MOVEMENT
    I gonna starrt on hiding in stuff

--- Assets/Scripts/LvlManager.cs
+++ Assets/Scripts/LvlManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class LvlManager : MonoBehaviour
 {
+
+    public GameObject mobileUI;
+    public bool showMobileUI;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mobileUI.SetActive(showMobileUI);    
     }
 
     // Update is called once per frame
--- Assets/Scripts/Scottie_Controller.cs
+++ Assets/Scripts/Scottie_Controller.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Scottie_Controller : MonoBehaviour
 {
-    public float MoveSpeed = 5f;                                                 //how fast the character moves
-
+    public float MoveSpeed;                                                      //Control Speed that the player is moving around the world
     private Rigidbody2D rb;                                                      //make a ref to the rigidbody2D component
     private SpriteRenderer sr;
     private float movement;
+    private float moveDirection = 0;
 
     private bool isHidden = false;                                               //is 'false' by default because we want the default tag to be "Player"
 
@@ -17,46 +18,38 @@ public class Scottie_Controller : MonoBehaviour
 
     AudioManager audioManager;                                                   //reference the Audio Manager Script
 
+    LvlManager thelvlManager;                                                    //Reference Level Manager
+
+    private SpriteRenderer rend;                                                 //define player's layer in the game
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();                                        //Get easy access to the Rigidbody2D component
         sr = GetComponent<SpriteRenderer>();                                     //Get easy access to the SpriteRenderer component
         myAnim = GetComponent<Animator>();                                       //Get easy access to the Animator component
+        thelvlManager = FindObjectOfType<LvlManager>();                          //Get reference for the level manager
     }
 
     void Update()
     {
-        /*Get horizontal input (-1 for left, 1 for right, 0 for none)
-        movement = Input.GetAxisRaw("Horizontal");
-
-        //For making sure the character faces the direction it's moving in
-        if (movement < 0)
+        if (!thelvlManager.showMobileUI)
         {
-            //moving left -> sprite flips to the left
-            sr.flipX = true;
+            Move(Input.GetAxisRaw("Horizontal"));                                    //Code for left right movement   
         }
-
-        if (movement > 0)
+        else
         {
-            //moving right -> sprite doesnt flip
-            sr.flipX = false;
+            Move(moveDirection);
         }
 
         //Toggle between "Player" and "Hidden" Tags in unity (so that the enemies can't detect the player)
         if (Input.GetKeyDown("e"))
         {
-            isHidden = !isHidden; //Changes 'isHidden' to true
+            isHidden = !isHidden;                                               //Changes 'isHidden' to true
             gameObject.tag = isHidden ? "Hidden" : "Player";                    //The "?" means "value if true" : "value if false". It's cleaner than a lot of if-else statements
-        }*/
+        }
 
         //Setting up Parameters in the Animator
-        myAnim.SetFloat("MoveSpeed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
-    }
-
-    void FixedUpdate()
-    {
-        // Move the character left or right
-        //rb.velocity = new Vector2(movement * MoveSpeed, rb.velocity.y);
+        myAnim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));                     //"Mathf.abs()" returns the value of velocity of rigidbody along the x axis
     }
 
     //Audio Scripts
@@ -68,32 +61,21 @@ public class Scottie_Controller : MonoBehaviour
 
     public void Move(float dir)
     {
-        //Get horizontal input (-1 for left, 1 for right, 0 for none)
-        //movement = Input.GetAxisRaw("Horizontal");
-
-        //For making sure the character faces the direction it's moving in
         if (dir > 0)
         {
-            //moving left -> sprite flips to the left
-            //sr.flipX = true;
-            rb.velocity = new Vector2(MoveSpeed, rb.velocity.y);
-            transform.localScale = new Vector3(1, 1, 1);
+            rb.velocity = new Vector2(MoveSpeed, rb.velocity.y);                 //Move right
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);                //When move right, face right
         }
         else if (dir < 0)
         {
-            rb.velocity = new Vector2(-MoveSpeed, rb.velocity.y);
-            transform.localScale = new Vector3(-1, 1, 1);
+            rb.velocity = new Vector2(-MoveSpeed, rb.velocity.y);                //Move Left
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);               //When move left, face left
         }
         else
         {
-            rb.velocity = new Vector2(0, rb.velocity.y);
+            rb.velocity = new Vector2(0, rb.velocity.y);                         //Stand still when input detects nothing
         }
-
-        /*if (dir > 0)
-        {
-            //moving right -> sprite doesnt flip
-            sr.flipX = false;
-        }*/
+        moveDirection = dir;
     }
     
 }
