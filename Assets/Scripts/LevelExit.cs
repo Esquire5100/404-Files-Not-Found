9a7b073f85d34540087fc956e8460c8a535310b8
Esquire5100 <KLEAH.24@ichat.sp.edu.sg>
2025-05-31 14:55:30 +0800

    Added in the Level 2 scene + the script

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player")) //If the Player hits the object...
+        {
+            SceneManager.LoadScene("Level 2"); //...then bring player to the next level
+        }
+    }
+}
