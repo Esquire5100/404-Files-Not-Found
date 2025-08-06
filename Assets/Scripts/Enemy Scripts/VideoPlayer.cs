5e0dda4be6700caf89b524770c029df4a072dfa8
Esquire5100 <KLEAH.24@ichat.sp.edu.sg>
2025-08-06 09:55:36 +0800

    Video Player script put in for future use

---
+++ Assets/Scripts/Enemy Scripts/VideoPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoTransition : MonoBehaviour
+{
+    VideoPlayer video;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += OnVideoEnd; // Event to handle when the video is finished
+    }
+
+    void OnVideoEnd(UnityEngine.Video.VideoPlayer vp)
+    {
+        // Replace '1' with the build index of your next scene
+        SceneManager.LoadScene("Game Over");
+    }
+}
