2493167895850ac49bf8b74dbdabc99a97b000f2
nurHusNINA07 <NHAKBAR.24@ichat.sp.edu.sg>
2025-08-21 19:16:45 +0800

    IT FADES BETWEEN LEVELS
    I think. There's a Controller bug I gotta fix rq to get the fading to work as intended. Right now, the Crossfade_End is fine, but the Crossfade_Start isn't.

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -7,14 +7,15 @@ using UnityEngine.UIElements;
 public class LevelExit : MonoBehaviour
 {
     public SoundEffectManager soundEffectManager;
-
     public string sceneToLoad;
+    //for crossfade transitions
+    public Animator transition;
+    public float transitionTime = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
          soundEffectManager = FindObjectOfType<SoundEffectManager>();
-
     }
 
     // Update is called once per frame
@@ -23,23 +24,32 @@ public class LevelExit : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
 
         if (other.CompareTag("Player")) //If the Player hits the object...
         {
             SoundEffectManager.Instance.StopMusic();
+            transition = GetComponent<Animator>();
+            //transition = GameObject.Find("FadingImage").GetComponent<Animator>();
+            StartCoroutine(levelLoad());
         }
 
-        if (LvlManager.Instance.CanExitLevel())
+        /*if (LvlManager.Instance.CanExitLevel())
         {
             SceneManager.LoadScene(sceneToLoad); //...then bring player to the next level
+            startCoroutine(levelLoad)();
         }
         else
         {
             Debug.Log($"Collect {LvlManager.Instance.filesRequired - LvlManager.Instance.FileCount} more files!");
-        }
+        }*/
     }
 
-
+    private IEnumerator levelLoad()
+        {
+            transition.SetTrigger("Start");
+            yield return new WaitForSeconds(transitionTime);
+            SceneManager.LoadScene(sceneToLoad);
+        }
 }
